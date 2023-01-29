69fd8dd26efa9c7a9e0647f40cd59bb68a1a785b
Douwe Pausma <douwepausma@gmail.com>
2023-01-29 18:33:21 +0100

    Added new components

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const { size } = require("lodash");
-
-module.exports = ({ addComponents, theme, e }) => {
+module.exports = ({ addComponents, addBase, theme, e }) => {
     const colors = theme('colors');
     let buttons = [];
     let badges = [];
+    let alerts = [];
+
+    let borderRadiusBase = '14px';
 
     /* Button base styles */
     buttons.push({
@@ -11,17 +12,17 @@ module.exports = ({ addComponents, theme, e }) => {
             display: 'inline-flex',
             alignItems: 'center',
             textAlign: 'center',
-            borderRadius: theme('borderRadius.xl'),
+            borderRadius: borderRadiusBase,
             fontWeight: '600',
             textDecorationLine: 'none',
-            transitionProperty: 'color, background-color',
+            transitionProperty: 'color, background-color, border-color',
             transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
             transitionDuration: '150ms',
             lineHeight: '1.5',
-            paddingTop: '5px',
-            paddingRight: theme('spacing.4'),
-            paddingBottom: '5px',
-            paddingLeft: theme('spacing.4'),
+            paddingTop: '6px',
+            paddingRight: '18px',
+            paddingBottom: '6px',
+            paddingLeft: '18px',
             border: '1px solid',
             borderColor: 'transparent',
             cursor: 'pointer',
@@ -31,52 +32,42 @@ module.exports = ({ addComponents, theme, e }) => {
     /* Button colors styles */
     buttons.push(Object.entries(colors).map(([key, value]) => {
         return {
-            [`.${e(`btn-${key}`)}`]: {
+            [`.btn-${key}`]: {
                 color: value[900],
                 backgroundColor: value[300],
+                borderColor: value[300],
 
                 '&:hover': {
                     backgroundColor: value[400],
+                    borderColor: value[500],
                 },
             },
         }
     }))
 
     /* Button outline styles */
-    buttons.push(
-        {
-            ['.btn-outline']: {
-                borderColor: theme('colors.gray.300'),
-                color: theme('colors.primary.900'),
+    buttons.push(Object.entries(colors).map(([key, value]) => {
+        return {
+            [`.btn-outline-${key}`]: {
+                backgroundColor: 'transparent',
+                borderColor: value[300],
+                color: value[900],
+
                 '&:hover': {
-                    borderColor: theme('colors.primary.300'),
-                    backgroundColor: theme('colors.primary.300'),
+                    backgroundColor: value[400],
+                    borderColor: value[500],
                 },
                 'path': {
-                    fill: theme('colors.primary.900'),
+                    fill: value[900],
                 }
             }
-        },
-        Object.entries(colors).map(([key, value]) => {
-            return {
-                [`.${e(`btn-outline-${key}`)}`]: {
-                    backgroundColor: 'transparent',
-                    borderColor: value[300],
-                    color: value[900],
-                    '&:hover': {
-                        backgroundColor: value[300],
-                    },
-                    'path': {
-                        fill: value[900],
-                    }
-                }
-            }
-        }))
+        }
+    }))
 
     /* Button icon styles */
     buttons.push({
         ['.btn-icon']: {
-            padding: '9px',
+            padding: '8px',
         }
     })
 
@@ -92,7 +83,7 @@ module.exports = ({ addComponents, theme, e }) => {
             borderRadius: theme('borderRadius.full'),
             fontWeight: '600',
             textDecorationLine: 'none',
-            transitionProperty: 'color, background-color',
+            transitionProperty: 'color, background-color, border-color',
             transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
             transitionDuration: '150ms',
             paddingRight: theme('spacing.2'),
@@ -104,15 +95,16 @@ module.exports = ({ addComponents, theme, e }) => {
     /* Badge colors styles */
     badges.push(Object.entries(colors).map(([key, value]) => {
         return {
-            [`.${e(`badge-${key}`)}`]: {
+            [`.badge-${key}`]: {
                 color: value[900],
                 backgroundColor: value[300],
             },
             /* Add hover styles when badge is <a> */
-            [`a.${e(`badge-${key}`)}`]: {
+            [`a.badge-${key}`]: {
                 '&:hover': {
                     backgroundColor: value[400],
-                    borderColor: value[400],
+                    borderColor: value[500],
+                    cursor: 'pointer',
                 },
             },
         }
@@ -121,15 +113,16 @@ module.exports = ({ addComponents, theme, e }) => {
     /* Badge outline colors styles */
     badges.push(Object.entries(colors).map(([key, value]) => {
         return {
-            [`.${e(`badge-outline-${key}`)}`]: {
+            [`.badge-outline-${key}`]: {
                 color: value[900],
                 borderColor: value[300],
             },
             /* Add hover styles when badge is <a> */
-            [`a.${e(`badge-outline-${key}`)}`]: {
+            [`a.badge-outline-${key}`]: {
                 '&:hover': {
-                    backgroundColor: value[300],
-                    borderColor: value[300],
+                    backgroundColor: value[400],
+                    borderColor: value[500],
+                    cursor: 'pointer',
                 },
             },
         }
@@ -137,18 +130,145 @@ module.exports = ({ addComponents, theme, e }) => {
 
     addComponents(badges);
 
+    /* Badge base styles */
+    alerts.push({
+        ['.alert']: {
+            display: 'flex',
+            alignItems: 'center',
+            textAlign: 'center',
+            borderRadius: theme('borderRadius.full'),
+            fontWeight: '600',
+            textDecorationLine: 'none',
+            transitionProperty: 'color, background-color, border-color',
+            transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+            transitionDuration: '150ms',
+            paddingTop: theme('spacing.2'),
+            paddingRight: theme('spacing.4'),
+            paddingBottom: theme('spacing.2'),
+            paddingLeft: theme('spacing.4'),
+            border: '1px solid transparent',
+        }
+    })
+
+    /* Badge colors styles */
+    alerts.push(Object.entries(colors).map(([key, value]) => {
+        return {
+            [`.alert-${key}`]: {
+                color: value[900],
+                backgroundColor: value[300],
+            },
+        }
+    }))
+
+    addComponents(alerts);
+
     /* Input styles */
     addComponents({
-        '.input-field': {
+        '.input-group': {
+            display: 'flex',
+            flexDirection: 'column',
+            marginBottom: theme('spacing.4'),
+
+            'label': {
+                display: 'flex',
+                alignItems: 'center',
+                fontWeight: '600',
+            },
+        },
+        '.input-field[type="radio"], .input-field[type="checkbox"]': {
+            marginRight: theme('spacing.2')
+        },
+        '.input-field[type="file"]': {
+            padding: '16px',
+            borderWidth: '1px',
+            borderColor: theme('colors.slate.400'),
+            borderRadius: '18px',
+            borderStyle: 'dashed',
+            marginTop: '2px',
+        },
+        '.input-field[type="text"], .input-field[type="email"], .input-field[type="number"], select.input-field, textarea.input-field': {
             width: '100%',
             borderWidth: '1px',
-            borderColor: colors.gray[300],
-            borderRadius: theme('borderRadius.xl'),
+            borderColor: colors.slate[400],
+            borderRadius: borderRadiusBase,
             backgroundColor: 'transparent',
-            paddingTop: '5px',
+            paddingTop: '6px',
+            paddingRight: '14px',
+            paddingBottom: '6px',
+            paddingLeft: '14px',
+            marginTop: '2px',
+
+            '&:focus-visible': {
+                borderColor: colors.primary[500],
+                outline: 'solid ' + colors.primary[300],
+            },
+            '&::placeholder': {
+                color: colors.slate[500],
+            },
+        },
+        'select.input-field': {
+            paddingRight: '30px',
+            webkitAppearance: 'none',
+            mozAppearance: 'none',
+            appearance: 'none',
+            backgroundImage: 'url(\'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"%3E%3Cpath fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd"/%3E%3C/svg%3E%0A\')',
+            backgroundSize: '18px',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'calc(100% - 0.75rem) center',
+        }
+    })
+
+    /* Alert base styles */
+    alerts.push({
+        ['.alert']: {
+            display: 'flex',
+            alignItems: 'center',
+            borderRadius: borderRadiusBase,
+            fontWeight: '600',
+            textDecorationLine: 'none',
+            transitionProperty: 'color, background-color, border-color',
+            transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+            transitionDuration: '150ms',
+            paddingTop: theme('spacing.2'),
             paddingRight: theme('spacing.3'),
-            paddingBottom: '5px',
+            paddingBottom: theme('spacing.2'),
             paddingLeft: theme('spacing.3'),
+            border: '1px solid transparent',
+        }
+    })
+
+    /* Alert colors styles */
+    alerts.push(Object.entries(colors).map(([key, value]) => {
+        return {
+            [`.alert-${key}`]: {
+                color: value[900],
+                backgroundColor: value[300],
+            },
+        }
+    }))
+
+    addComponents(alerts);
+
+    /* Pre, Code styles */
+    addBase({
+        'pre': {
+            borderRadius: borderRadiusBase,
+            fontSize: '0.875rem',
+            color: colors.white,
+            borderColor: colors.gray[900],
+            borderWidth: '1px',
+            backgroundColor: colors.gray[700],
+            paddingTop: '10px',
+            paddingRight: theme('spacing.6'),
+            paddingBottom: '10px',
+            paddingLeft: theme('spacing.6'),
+        },
+        'code': {
+            backgroundColor: theme('colors.primary.100'),
+            color: theme('colors.primary.900'),
+            paddingRight: theme('spacing.1'),
+            paddingLeft: theme('spacing.1'),
+            borderRadius: '4px'
         }
     })
 }
